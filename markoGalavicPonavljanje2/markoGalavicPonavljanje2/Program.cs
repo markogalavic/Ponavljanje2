3dda74d558b71c6419296989a82c18d8ab8f09cc
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-11-12 10:30:58 +0100

    Add files via upload

---
+++ markoGalavicPonavljanje2/markoGalavicPonavljanje2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavicPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite stringove");
+            string znakovi = Console.ReadLine();
+
+            Console.WriteLine(znakovi.Replace(" ","_"));
+            Console.ReadKey();
+        }
+    }
+}
